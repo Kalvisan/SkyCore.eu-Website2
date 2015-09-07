c6a85e2c3c99385eed8399be0b091fab0ff6dbd8
BuildTools <unconfigured@null.spigotmc.org>
2015-09-07 22:24:29 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>SkyCore | Comunity</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+		<link rel="icon" type="image/png" href="http://skycore.eu/images/logo.ico">
+		
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+		</noscript>
+	</head>
+	<body class="landing">
+
+		<!-- Header -->
+			<header id="header">
+				<h1><img href="index.html" src="/images/logo.png" style="padding: 20px"></img></h1>
+				<nav id="nav">
+					<ul>
+						<li><a href="index.html">Sākums</a></li>
+						<li><a href="/f">Forums</a></li>
+						<li><a href="/vote/">Balsošana</a></li>
+						<li><a href="/statics">Stati</a></li>
+					</ul>
+				</nav>
+			</header>
+
+		<!-- Banner -->
+			<section id="banner">
+			<video>
+				<source type="video/mp4" src="http://skycore.eu/images/clouds.mp4">
+			</video>
+			
+				<h2>SkyCore projekts</h2>
+				<p><b>Minecraft servera ip:</b> mc.skycore.eu</p>
+				<ul class="actions">
+					<li>
+						<a href="http://skycore.eu/f/index.php?app=core&module=global&section=login" class="button big">Pievienojies musu projektā</a>
+					</li>
+				</ul>
+			</section>
+
+		<!-- One -->
+			<section id="one" class="wrapper style1 special">
+				<div class="container">
+					<div class="row 200%">
+						<div class="4u 12u$(medium)">
+							<section class="box">
+								<a href="/f" class="icon big rounded color1 fa-cloud"></a>
+								<h3>Forums</h3>
+							</section>
+						</div>
+						<div class="4u 12u$(medium)">
+							<section class="box">
+								<a href="/vote" class="icon big rounded color9 fa-check"></a>
+								<h3>Balsošana</h3>
+							</section>
+						</div>
+						<div class="4u$ 12u$(medium)">
+							<section class="box">
+								<a href="/statics" class="icon big rounded color6 fa-bullseye"></a>
+								<h3>Spelētāju panelis</h3>
+							</section>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		<!-- Two -->
+			<section id="two" class="wrapper style2 special" style="background: #fff;">
+				<div class="container">
+					<header class="major">
+						<h2>Spēlētāju TOP 10</h2>
+						<p>Minecraft labāko spēlētāju top 10.</p>
+					</header>
+					<section class="profiles">
+						<div class="row">
+							<section class="3u 3u(medium) 12u(xsmall) profile">
+								<img src="http://cravatar.eu/avatar/KechupsLv/90.png" alt="" />
+								<h4>KechupsLv</h4>
+								<p>Punkti: 100</p>
+							</section>
+							<section class="3u 3u(medium) 12u(xsmall) profile">
+								<img src="http://cravatar.eu/avatar/Rokijs/90.png" alt="" />
+								<h4>Rokijs</h4>
+								<p>Punkti: 50</p>
+							</section>
+							<section class="3u 3u(medium) 12u(xsmall) profile">
+								<img src="http://cravatar.eu/avatar/Player/90.png" alt="" />
+								<h4>Player</h4>
+								<p>Punkti: 10</p>
+							</section>
+							<section class="3u 3u(medium) 12u(xsmall) profile">
+								<img src="http://cravatar.eu/avatar/Lox/90.png" alt="" />
+								<h4>Lox</h4>
+								<p>Punkti: 2</p>
+							</section>
+							<section class="3u 3u(medium) 12u(xsmall) profile">
+								<img src="http://cravatar.eu/avatar/Notch/90.png" alt="" />
+								<h4>Notch</h4>
+								<p>Punkti: 1</p>
+							</section>
+							<section class="3u 3u(medium) 12u(xsmall) profile">
+								<img src="http://cravatar.eu/avatar/KechupsLv/90.png" alt="" />
+								<h4>KechupsLv</h4>
+								<p>Punkti: 100</p>
+							</section>
+							<section class="3u 3u(medium) 12u(xsmall) profile">
+								<img src="http://cravatar.eu/avatar/Rokijs/90.png" alt="" />
+								<h4>Rokijs</h4>
+								<p>Punkti: 50</p>
+							</section>
+							<section class="3u 3u(medium) 12u(xsmall) profile">
+								<img src="http://cravatar.eu/avatar/Player/90.png" alt="" />
+								<h4>Player</h4>
+								<p>Punkti: 10</p>
+							</section>
+							<section class="3u 3u(medium) 12u()xsmall) profile">
+								<img src="http://cravatar.eu/avatar/Lox/90.png" alt="" />
+								<h4>Lox</h4>
+								<p>Punkti: 2</p>
+							</section>
+							<section class="3u 3u$(medium) 12u$(xsmall) profile">
+								<img src="http://cravatar.eu/avatar/Notch/90.png" alt="" />
+								<h4>Notch</h4>
+								<p>Punkti: 1</p>
+							</section>
+						</div>
+					</section>
+					<footer>
+						<ul class="actions">
+							<li>
+								<a href="/statics" class="button big">Skatītīes visus status</a>
+							</li>
+						</ul>
+					</footer>
+				</div>
+			</section>
+
+		<!-- Three -->
+			<section id="three" class="wrapper style3 special">
+				<div class="container">
+					<header class="major">
+						<h2>Sazinies ar mums</h2>
+						<p>Tiešā vēstule SkyCore darbiniekiem</p>
+					</header>
+				</div>
+				<div class="container 50%">
+				<div class="monitor_block" style="background:url('http://noob.lv/template/nooblv_original/template_images/maps/minecraft/world.png')" >
+					<div class="monitor_info">
+						<div class="monitor_info_1 pull-left radius3_top-right radius3_bottom-right">mc.skycore.eu</div>
+							<div class="monitor_players pull-left radius3_top-right radius3_bottom-right"><b>22</b> / 100</div>
+							<div class="monitor_info_2 pull-left radius3_top-right radius3_bottom-right">
+						</div>
+					</div>
+				</div>
+					<?php
+						include_once 'MinecraftServerStatus/status.class.php';
+						$status = new MinecraftServerStatus();
+						$response = $status->getStatus('skycore.eu',25565,'1.8');
+						if(!$response) {
+							echo"The Server is offline!";
+						} else {
+							echo"<img width=\"64\" height=\"64\" src=\"".$response['favicon']."\" /> <br>
+							The Server ".$response['hostname']." is running on ".$response['version']." and is online,
+							currently are ".$response['players']." players online
+							of a maximum of ".$response['maxplayers'].". The motd of the server is '".$response['motd']."'. 
+							The server has a ping of ".$response['ping']." milliseconds.";
+						}
+						if ($response['players'] > 0) {
+						$players = "Serverī pieslēdzies ".$response['players']." spēlētājs";
+						 
+						} elseif ($response['players'] == 0) {
+						$players = "Serveris ir tukš!";
+						}
+						 
+						echo "
+						".$players;
+					?>
+				</div>
+			</section>
+			
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="container">
+					<div class="row">
+						<div class="8u 12u$(medium)">
+							<ul class="copyright">
+								<li>&copy; SkyCore.Eu All rights reserved.</li>
+								<li>Admins: <a href="http://skycore.eu/f/index.php?/user/1-kechupslv/">KechupsLv</a>, <a href="http://skycore.eu/f/index.php?/user/2-simple/">Simple</a></li>
+							</ul>
+						</div>
+						<div class="4u$ 12u$(medium)">
+							<ul class="icons">
+								<li>
+									<a class="icon rounded fa-facebook"><span class="label">Facebook</span></a>
+								</li>
+								<li>
+									<a class="icon rounded fa-twitter"><span class="label">Twitter</span></a>
+								</li>
+								<li>
+									<a class="icon rounded fa-google-plus"><span class="label">Google+</span></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</footer>
+	</body>
+</html>
